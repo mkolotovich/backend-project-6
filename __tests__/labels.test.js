7189e64228d59eb89fa8d7af48218cbdb51b5f40
Mike Kolotovich <k2punk@gmail.com>
2023-04-21 13:09:47 +0300

    fix route in labels edit

--- __tests__/labels.test.js
+++ __tests__/labels.test.js
@@ -12,7 +12,10 @@ describe('test labels CRUD', () => {
   const testData = getTestData();
 
   beforeAll(async () => {
-    app = fastify({ logger: { prettyPrint: true } });
+    app = fastify({
+      exposeHeadRoutes: false,
+      logger: { target: 'pino-pretty' },
+    });
     await init(app);
     knex = app.objection.knex;
     models = app.objection.models;
